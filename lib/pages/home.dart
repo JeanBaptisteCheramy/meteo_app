1f8ed361eb5be0003459ff040de3e8d5bbe23152
JeanBaptisteCheramy <jeanbaptiste.cheramy@gmail.com>
2024-05-20 14:41:13 +0200

    layout setup WIP

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:meteo_app/widgets/header_section.dart';
 
 var logger = Logger();
 
@@ -13,19 +14,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 53, 85, 181),
-                Color.fromARGB(255, 42, 12, 81),
-              ],
-              begin: FractionalOffset(0.0, 0.0),
-              end: FractionalOffset(0.0, 0.9),
-              stops: [0.0, 1.0],
-              tileMode: TileMode.clamp),
-        ),
+        body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: const EdgeInsets.fromLTRB(20, 80, 20, 20),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 53, 85, 181),
+              Color.fromARGB(255, 42, 12, 81),
+            ],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(0.0, 0.9),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
       ),
-    );
+      child: const Column(
+        children: [Header()],
+      ),
+    ));
   }
 }
