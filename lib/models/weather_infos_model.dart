1225c1201b3700a39ebc10fd7f826cfd84e91860
JeanBaptisteCheramy <jeanbaptiste.cheramy@gmail.com>
2024-05-24 21:37:26 +0200

    starting weather_info_model

---
+++ lib/models/weather_infos_model.dart
@@ -0,0 +1,9 @@
+class WeatherInfos {
+  final Map<String, int> coord = {};
+  final String weather = '';
+  final Map<String, int> main = {};
+  final Map<String, int> wind = {};
+  final String clouds = '';
+  final Map<String, int> sys = {};
+  final String name = '';
+}
