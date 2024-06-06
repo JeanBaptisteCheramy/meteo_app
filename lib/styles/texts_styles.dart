97c34849057c42e93b651d2c50c58370bd04b2cf
JeanBaptisteCheramy <jeanbaptiste.cheramy@gmail.com>
2024-06-06 17:12:38 +0200

    font add

---
+++ lib/styles/texts_styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class TextsStyles {
+  static const titleXL = TextStyle(
+      color: Color.fromARGB(255, 249, 245, 255),
+      shadows: [
+        Shadow(
+          blurRadius: 10.0, // shadow blur
+          color: Color.fromARGB(194, 0, 0, 0), // shadow color
+          offset: Offset(2.0, 2.0), // how much shadow will be shown
+        ),
+      ],
+      fontFamily: 'Maven',
+      fontSize: 50,
+      fontWeight: FontWeight.w400);
+  static const titleMD = TextStyle(
+      color: Color.fromARGB(255, 249, 245, 255),
+      fontFamily: 'Maven',
+      fontSize: 32,
+      fontWeight: FontWeight.w400);
+  static const content = TextStyle(
+      color: Color.fromARGB(255, 249, 245, 255),
+      fontFamily: 'Maven',
+      fontSize: 18,
+      fontWeight: FontWeight.w400);
+}
--- lib/widgets/header_section.dart
+++ lib/widgets/header_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meteo_app/services/services.dart';
+import 'package:meteo_app/styles/texts_styles.dart';
 
 class Header extends StatelessWidget {
   const Header(this.data, {super.key});
@@ -16,63 +17,59 @@ class Header extends StatelessWidget {
             image: AssetImage(
                 'assets/images/${backImagePicker(data['weather']['weather'][0]['main'], data['weather']['sys']['sunset'], data['weather']['sys']['sunrise'])}'),
             fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.white.withOpacity(0.8),
+              BlendMode.dstATop,
+            ),
           ),
           color: const Color.fromARGB(48, 132, 197, 254),
         ),
         child: Column(children: [
           SizedBox(
-            height: 50,
-            child: Text(
-              data['city'],
-              style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-            ),
+            height: 80,
+            child: Text(data['city'], style: TextsStyles.titleXL),
           ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(132, 120, 117, 197),
-              borderRadius: BorderRadius.circular(40),
-            ),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+          FractionallySizedBox(
+              widthFactor: 0.8,
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(121, 0, 0, 0),
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                child: Column(
                   children: [
-                    Image(
-                      image: AssetImage(weatherImagePicker(
-                          data['weather']['weather'][0]['id'])),
-                      height: 50,
-                      width: 50,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage(weatherImagePicker(
+                              data['weather']['weather'][0]['id'])),
+                          height: 50,
+                          width: 50,
+                        ),
+                        Text(
+                            "${kelvinToDeg(data['weather']['main']['temp'])}°C",
+                            style: TextsStyles.titleMD),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                            "Min: ${kelvinToDeg(data['weather']['main']['temp_min'])}°C",
+                            style: TextsStyles.content),
+                        Text(
+                            "Max: ${kelvinToDeg(data['weather']['main']['temp_max'])}°C",
+                            style: TextsStyles.content),
+                      ],
                     ),
                     Text(
-                      "${kelvinToDeg(data['weather']['main']['temp'])}°C",
-                      style:
-                          const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                    ),
+                        "Vent: ${msConversion(data['weather']['wind']['speed'])}km/h",
+                        style: TextsStyles.content),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Min: ${kelvinToDeg(data['weather']['main']['temp_min'])}°C",
-                      style:
-                          const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                    ),
-                    Text(
-                      "Max: ${kelvinToDeg(data['weather']['main']['temp_max'])}°C",
-                      style:
-                          const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                    ),
-                  ],
-                ),
-                Text(
-                  "Vent: ${msConversion(data['weather']['wind']['speed'])}km/h",
-                  style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                ),
-              ],
-            ),
-          )
+              ))
         ]));
   }
 }
