378696125267033d3775a2eb2717073eafd280c2
JeanBaptisteCheramy <jeanbaptiste.cheramy@gmail.com>
2024-06-07 18:23:26 +0200

    glassmorphism test

--- lib/widgets/mid_section.dart
+++ lib/widgets/mid_section.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meteo_app/services/services.dart';
+import 'package:glassmorphism_ui/glassmorphism_ui.dart';
 import 'package:meteo_app/widgets/forecast_card.dart';
 
 class MidSection extends StatelessWidget {
@@ -8,8 +8,24 @@ class MidSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
+    return GlassContainer(
+      height: 200,
+      // width: 200,
+      blur: 4,
+      color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.6),
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Colors.white.withOpacity(0.2),
+          Colors.blue.withOpacity(0.3),
+        ],
+      ),
+      //--code to remove border
+      border: const Border.fromBorderSide(BorderSide.none),
+      shadowStrength: 5,
+
+      shadowColor: Colors.white.withOpacity(0.6),
       child: Row(children: [ForecastCard(data['forecast'][0])]),
     );
   }
